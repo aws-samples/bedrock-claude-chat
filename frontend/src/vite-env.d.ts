6ded88680767142da8e103a854bac08593cd1e1c
kyosuke-kobayashi-lvgs <kyosuke.kobayashi@leverages.jp>
2024-03-05 18:23:29 +0900

    update vite-env

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -6,6 +6,8 @@ interface ImportMetaEnv {
   readonly VITE_APP_REGION: string;
   readonly VITE_APP_USER_POOL_ID: string;
   readonly VITE_APP_USER_POOL_CLIENT_ID: string;
+  readonly VITE_APP_REDIRECT_SIGNIN_URL: string;
+  readonly VITE_APP_REDIRECT_SIGNOUT_URL: string;
 }
 
 interface ImportMeta {
