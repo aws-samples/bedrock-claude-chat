c2b528daf94697f07c9f44d67d97c3a821181dbf
kyosuke-kobayashi-lvgs <kyosuke.kobayashi@leverages.jp>
2024-03-11 14:07:17 +0900

    Support for multiple IDPs

--- cdk/lib/constructs/auth.ts
+++ cdk/lib/constructs/auth.ts
@@ -18,6 +18,27 @@ export class Auth extends Construct {
   constructor(scope: Construct, id: string, props: AuthProps) {
     super(scope, id);
 
+    const providers: [] = this.node.tryGetContext("identifyProviders");
+    const supportedIndetityProviders = [
+      ...providers,
+      { service: "cognito" },
+    ].map(({ service }) => {
+      switch (service) {
+        case "google":
+          return aws_cognito.UserPoolClientIdentityProvider.GOOGLE;
+        case "facebook":
+          return aws_cognito.UserPoolClientIdentityProvider.FACEBOOK;
+        case "amazon":
+          return aws_cognito.UserPoolClientIdentityProvider.AMAZON;
+        case "apple":
+          return aws_cognito.UserPoolClientIdentityProvider.APPLE;
+        case "cognito":
+          return aws_cognito.UserPoolClientIdentityProvider.COGNITO;
+        default:
+          return aws_cognito.UserPoolClientIdentityProvider.COGNITO;
+      }
+    });
+
     const userPool = new UserPool(this, "UserPool", {
       passwordPolicy: {
         requireUppercase: true,
@@ -31,40 +52,56 @@ export class Auth extends Construct {
         email: true,
       },
     });
+
     const userPoolDomainPrefixKey: string = this.node.tryGetContext(
       "userPoolDomainPrefix"
     );
+
+    // unused, but required for Google auth
     userPool.addDomain("UserPool", {
       cognitoDomain: {
         domainPrefix: userPoolDomainPrefixKey,
       },
     });
 
-    const client = userPool.addClient(`Client`, {
-      idTokenValidity: Duration.days(1),
-      authFlows: {
-        userPassword: true,
-        userSrp: true,
-      },
-      oAuth: {
-        callbackUrls: [props.origin],
-        logoutUrls: [props.origin],
-      },
-      supportedIdentityProviders: [
-        aws_cognito.UserPoolClientIdentityProvider.GOOGLE,
-        aws_cognito.UserPoolClientIdentityProvider.COGNITO,
-      ],
-    });
+    const clientProps = (() => {
+      const defaultProps = {
+        idTokenValidity: Duration.days(1),
+        authFlows: {
+          userPassword: true,
+          userSrp: true,
+        },
+      };
+      if (providers.length == 0) return defaultProps;
+      return {
+        ...defaultProps,
+        oAuth: {
+          callbackUrls: [props.origin],
+          logoutUrls: [props.origin],
+        },
+        supportedIdentityProviders: [...supportedIndetityProviders],
+      };
+    })();
 
+    const client = userPool.addClient(`Client`, clientProps);
+
+    const googleAuthSecretName: string = this.node.tryGetContext(
+      "googleAuthSecretName"
+    );
+    const googleProviderClientId: string = this.node.tryGetContext(
+      "googleProviderClientId"
+    );
+
+    // unused, but required for Google auth
     const googleProvider = new UserPoolIdentityProviderGoogle(
       this,
       "GoogleProvider",
       {
         userPool: userPool,
-        clientId: SecretValue.secretsManager("GoogleProviderClientId", {
+        clientId: SecretValue.secretsManager(googleProviderClientId, {
           jsonField: "clientId",
         }).unsafeUnwrap(),
-        clientSecretValue: SecretValue.secretsManager("GoogleAuthSecret", {
+        clientSecretValue: SecretValue.secretsManager(googleAuthSecretName, {
           jsonField: "clientSecret",
         }),
         scopes: ["openid", "email"],
@@ -77,6 +114,7 @@ export class Auth extends Construct {
     this.client = client;
     this.userPool = userPool;
 
+    // unused, but required for Google auth
     client.node.addDependency(googleProvider);
 
     new CfnOutput(this, "UserPoolId", { value: userPool.userPoolId });
