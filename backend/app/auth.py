c103440fd0b0514ba2e2161fef3d5e47fe06c9ac
kyosuke-kobayashi-lvgs <kyosuke.kobayashi@leverages.jp>
2024-03-22 17:26:45 +0900

    update comment

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -15,6 +15,10 @@ def verify_token(token: str) -> dict:
     keys = response.json()["keys"]
     header = jwt.get_unverified_header(token)
     key = [k for k in keys if k["kid"] == header["kid"]][0]
-    # The JWT returned from the Identity Provider may contain an at_hash, so we will disable the verify_at_hash check.
+    # The JWT returned from the Identity Provider may contain an at_hash
+    # jose jwt.decode verifies id_token with access_token by default if it contains at_hash
+    # FYI : https://github.com/mpdavis/python-jose/blob/4b0701b46a8d00988afcc5168c2b3a1fd60d15d8/jose/jwt.py#L59
+    # The access_token is not included in the front-end request header, so at_hash is ignored.
+    # so we will disable the verify_at_hash check.
     decoded = jwt.decode(token, key, algorithms=["RS256"], options={"verify_at_hash": False }, audience=CLIENT_ID)
     return decoded
