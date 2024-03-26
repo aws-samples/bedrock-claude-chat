639bd5af95c280cbabf6894acc2f86b36c91b3d4
kyosuke-kobayashi-lvgs <kyosuke.kobayashi@leverages.jp>
2024-03-26 16:13:05 +0900

    wip def red test

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -4,8 +4,42 @@ import { BedrockChatStack } from "../lib/bedrock-chat-stack";
 
 describe("Snapshot Test", () => {
   const app = new cdk.App();
-  test("Identity Provider Generation", () => {});
-  test("default stack", () => {});
+  test("Identity Provider Generation", () => {
+    const hasGoogleProviderStack = new BedrockChatStack(app, "MyTestStack", {
+      bedrockRegion: "us-east-1",
+      crossRegionReferences: true,
+      webAclId: "",
+      enableUsageAnalysis: true,
+      identityProviders: [
+        {
+          secretName: "MyTestSecret",
+          service: "google",
+        },
+      ],
+      userPoolDomainPrefix: "test-domain",
+      publishedApiAllowedIpV4AddressRanges: [""],
+      publishedApiAllowedIpV6AddressRanges: [""],
+    });
+    const hasGooglePRoviderTemplate = Template.fromStack(
+      hasGoogleProviderStack
+    ).toJSON();
+    expect(hasGooglePRoviderTemplate).toMatchSnapshot();
+  });
+
+  test("default stack", () => {
+    const stack = new BedrockChatStack(app, "MyTestStack", {
+      bedrockRegion: "us-east-1",
+      crossRegionReferences: true,
+      webAclId: "",
+      enableUsageAnalysis: true,
+      identityProviders: [],
+      userPoolDomainPrefix: "",
+      publishedApiAllowedIpV4AddressRanges: [""],
+      publishedApiAllowedIpV6AddressRanges: [""],
+    });
+    const template = Template.fromStack(stack).toJSON();
+    expect(template).toMatchSnapshot();
+  });
 });
 
 describe("Fine-grained Assertions Test", () => {
@@ -13,6 +47,43 @@ describe("Fine-grained Assertions Test", () => {
 });
 
 describe("Error handling", () => {
-  test("Invalid service name", () => {});
-  test("Lack of userPoolPrefix definition during Identity Provider generation");
+  const app = new cdk.App();
+
+  test("Invalid service name", () => {
+    const invalidProviderStack = new BedrockChatStack(app, "MyTestStack", {
+      bedrockRegion: "us-east-1",
+      crossRegionReferences: true,
+      webAclId: "",
+      enableUsageAnalysis: true,
+      identityProviders: [
+        {
+          secretName: "MyTestSecret",
+          service: "openai",
+        },
+      ],
+      userPoolDomainPrefix: "test-domain",
+      publishedApiAllowedIpV4AddressRanges: [""],
+      publishedApiAllowedIpV6AddressRanges: [""],
+    });
+
+    // expect
+  });
+  test("Lack of userPoolPrefix definition during Identity Provider generation", () => {
+    const invalidProviderStack = new BedrockChatStack(app, "MyTestStack", {
+      bedrockRegion: "us-east-1",
+      crossRegionReferences: true,
+      webAclId: "",
+      enableUsageAnalysis: true,
+      identityProviders: [
+        {
+          secretName: "MyTestSecret",
+          service: "google",
+        },
+      ],
+      userPoolDomainPrefix: "",
+      publishedApiAllowedIpV4AddressRanges: [""],
+      publishedApiAllowedIpV6AddressRanges: [""],
+    });
+    // expect
+  });
 });
