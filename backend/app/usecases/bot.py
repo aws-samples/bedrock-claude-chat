934b954d9b390bb434c732df8c4eaed002505a54
2814109 <2814109k@gmail.com>
2024-03-28 18:55:55 +0900

    update has_knowledge

--- backend/app/usecases/bot.py
+++ backend/app/usecases/bot.py
@@ -76,10 +76,12 @@ def create_new_bot(user_id: str, bot_input: BotInput) -> BotOutput:
     Bot is created as private and not pinned.
     """
     current_time = get_current_time()
-    has_knowledge = bot_input.knowledge and (
+    has_knowledge = bot_input.knowledge and bot_input.embedding_params and (
         len(bot_input.knowledge.source_urls) > 0
         or len(bot_input.knowledge.sitemap_urls) > 0
         or len(bot_input.knowledge.filenames) > 0
+        or len(bot_input.embedding_params.chunk_size) > 0
+        or len(bot_input.embedding_params.chunk_overlap) > 0
     )
     sync_status: type_sync_status = "QUEUED" if has_knowledge else "SUCCEEDED"
 
@@ -112,6 +114,7 @@ def create_new_bot(user_id: str, bot_input: BotInput) -> BotOutput:
             public_bot_id=None,
             is_pinned=False,
             owner_user_id=user_id,  # Owner is the creator
+            embedding_params= EmbeddingParamsModel(chunk_size=bot_input.chunk_size, chunk_overlap=bot_input.chunk_overlap)
             knowledge=KnowledgeModel(
                 source_urls=source_urls, sitemap_urls=sitemap_urls, filenames=filenames
             ),
