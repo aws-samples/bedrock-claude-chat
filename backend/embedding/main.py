0b54f6d20d5ff55e28547933068e401187a0fd12
2814109 <2814109k@gmail.com>
2024-03-29 12:13:30 +0900

    add print

--- backend/embedding/main.py
+++ backend/embedding/main.py
@@ -244,5 +244,8 @@ if __name__ == "__main__":
     print(f"source_urls to crawl: {source_urls}")
     print(f"sitemap_urls to crawl: {sitemap_urls}")
     print(f"filenames: {filenames}")
+    print(f"chunk_size: {chunk_size}")
+    print(f"chunk_overlap: {chunk_overlap}")
+
 
     main(user_id, bot_id, sitemap_urls, source_urls, filenames, chunk_size, chunk_overlap)
