b6700a339c41298317a5b86e191c602b32042673
2814109 <2814109k@gmail.com>
2024-04-01 19:52:35 +0900

    commonize

--- backend/tests/usecases/test_bot.py
+++ backend/tests/usecases/test_bot.py
@@ -4,6 +4,8 @@ import sys
 sys.path.append(".")
 import unittest
 
+from pydantic import BaseModel
+
 from app.config import EMBEDDING_CONFIG
 
 from app.repositories.custom_bot import (
@@ -34,126 +36,46 @@ class TestIssuePresignedUrl(unittest.TestCase):
         self.assertTrue(url.startswith("https://"))
 
 
-class TestFindAllBots(unittest.IsolatedAsyncioTestCase):
-    def setUp(self) -> None:
-        bot1 = BotModel(
-            id="1",
-            title="Test Bot",
-            description="Test Bot Description",
-            instruction="Test Bot Prompt",
-            create_time=1627984879.9,
-            last_used_time=1627984879.9,
-            # Pinned
-            is_pinned=True,
-            public_bot_id=None,
-            owner_user_id="user1",
-            embedding_params=EmbeddingParamsModel(
-                chunk_size=EMBEDDING_CONFIG["chunk_size"],
-                chunk_overlap=EMBEDDING_CONFIG["chunk_overlap"],
-            ),
-            knowledge=KnowledgeModel(
-                source_urls=["https://aws.amazon.com/"],
-                sitemap_urls=["https://aws.amazon.sitemap.xml"],
-                filenames=["test.txt"],
-            ),
-            sync_status="RUNNING",
-            sync_status_reason="reason",
-            sync_last_exec_id="",
-            published_api_stack_name=None,
-            published_api_datetime=None,
-            published_api_codebuild_id=None,
-        )
-        bot2 = BotModel(
-            id="2",
-            title="Test Bot",
-            description="Test Bot Description",
-            instruction="Test Bot Prompt",
-            create_time=1627984879.9,
-            last_used_time=1627984879.9,
-            # Pinned
-            is_pinned=True,
-            public_bot_id=None,
-            owner_user_id="user1",
-            embedding_params=EmbeddingParamsModel(
-                chunk_size=EMBEDDING_CONFIG["chunk_size"],
-                chunk_overlap=EMBEDDING_CONFIG["chunk_overlap"],
-            ),
-            knowledge=KnowledgeModel(
-                source_urls=["https://aws.amazon.com/"],
-                sitemap_urls=["https://aws.amazon.sitemap.xml"],
-                filenames=["test.txt"],
-            ),
-            sync_status="RUNNING",
-            sync_status_reason="reason",
-            sync_last_exec_id="",
-            published_api_stack_name=None,
-            published_api_datetime=None,
-            published_api_codebuild_id=None,
-        )
-        bot3 = BotModel(
-            id="3",
-            title="Test Bot",
-            description="Test Bot Description",
-            instruction="Test Bot Prompt",
-            create_time=1627984879.9,
-            last_used_time=1627984879.9,
-            # Not Pinned
-            is_pinned=False,
-            public_bot_id=None,
-            owner_user_id="user1",
-            embedding_params=EmbeddingParamsModel(
-                chunk_size=EMBEDDING_CONFIG["chunk_size"],
-                chunk_overlap=EMBEDDING_CONFIG["chunk_overlap"],
-            ),
-            knowledge=KnowledgeModel(
-                source_urls=["https://aws.amazon.com/"],
-                sitemap_urls=["https://aws.amazon.sitemap.xml"],
-                filenames=["test.txt"],
-            ),
-            sync_status="RUNNING",
-            sync_status_reason="reason",
-            sync_last_exec_id="",
-            published_api_stack_name=None,
-            published_api_datetime=None,
-            published_api_codebuild_id=None,
-        )
-        bot4 = BotModel(
-            id="4",
-            title="Test Bot",
-            description="Test Bot Description",
-            instruction="Test Bot Prompt",
-            create_time=1627984879.9,
-            last_used_time=1627984879.9,
-            # Not Pinned
-            is_pinned=False,
-            public_bot_id=None,
-            owner_user_id="user1",
-            embedding_params=EmbeddingParamsModel(
-                chunk_size=EMBEDDING_CONFIG["chunk_size"],
-                chunk_overlap=EMBEDDING_CONFIG["chunk_overlap"],
-            ),
-            knowledge=KnowledgeModel(
-                source_urls=["https://aws.amazon.com/"],
-                sitemap_urls=["https://aws.amazon.sitemap.xml"],
-                filenames=["test.txt"],
-            ),
-            sync_status="RUNNING",
-            sync_status_reason="reason",
-            sync_last_exec_id="",
-            published_api_stack_name=None,
-            published_api_datetime=None,
-            published_api_codebuild_id=None,
-        )
-        public_bot1 = BotModel(
-            id="public1",
+def create_private_bot(id, is_pinned,owner_user_id):
+    return BotModel(
+        id=id,
+        title="Test Bot",
+        description="Test Bot Description",
+        instruction="Test Bot Prompt",
+        create_time=1627984879.9,
+        last_used_time=1627984879.9,
+        # Pinned
+        is_pinned=is_pinned,
+        public_bot_id=None,
+        owner_user_id=owner_user_id,
+        embedding_params=EmbeddingParamsModel(
+            chunk_size=EMBEDDING_CONFIG["chunk_size"],
+            chunk_overlap=EMBEDDING_CONFIG["chunk_overlap"],
+        ),
+        knowledge=KnowledgeModel(
+            source_urls=["https://aws.amazon.com/"],
+            sitemap_urls=["https://aws.amazon.sitemap.xml"],
+            filenames=["test.txt"],
+        ),
+        sync_status="RUNNING",
+        sync_status_reason="reason",
+        sync_last_exec_id="",
+        published_api_stack_name=None,
+        published_api_datetime=None,
+        published_api_codebuild_id=None,
+    )
+
+def create_public_bot(id,is_pinned, owner_user_id):
+    return BotModel(
+            id=id,
             title="Test Public Bot",
             description="Test Public Bot Description",
             instruction="Test Public Bot Prompt",
             create_time=1627984879.9,
             last_used_time=1627984879.9,
-            is_pinned=True,
+            is_pinned=is_pinned,
             public_bot_id=None,
-            owner_user_id="user2",
+            owner_user_id=owner_user_id,
             embedding_params=EmbeddingParamsModel(
                 chunk_size=EMBEDDING_CONFIG["chunk_size"],
                 chunk_overlap=EMBEDDING_CONFIG["chunk_overlap"],
@@ -170,78 +92,57 @@ class TestFindAllBots(unittest.IsolatedAsyncioTestCase):
             published_api_datetime=None,
             published_api_codebuild_id=None,
         )
-        public_bot2 = BotModel(
-            id="public2",
-            title="Test Public Bot",
-            description="Test Public Bot Description",
-            instruction="Test Public Bot Prompt",
-            create_time=1627984879.9,
-            last_used_time=1627984879.9,
-            is_pinned=True,
-            public_bot_id=None,
-            owner_user_id="user2",
-            embedding_params=EmbeddingParamsModel(
-                chunk_size=EMBEDDING_CONFIG["chunk_size"],
-                chunk_overlap=EMBEDDING_CONFIG["chunk_overlap"],
-            ),
-            knowledge=KnowledgeModel(
-                source_urls=["https://aws.amazon.com/"],
-                sitemap_urls=["https://aws.amazon.sitemap.xml"],
-                filenames=["test.txt"],
-            ),
-            sync_status="RUNNING",
-            sync_status_reason="reason",
-            sync_last_exec_id="",
-            published_api_stack_name=None,
-            published_api_datetime=None,
-            published_api_codebuild_id=None,
-        )
-        alias1 = BotAliasModel(
-            id="alias1",
+    
+def create_bot_alias(id,original_bot_id,is_pinned):
+    return BotAliasModel(
+            id=id,
             # Different from original. Should be updated after `fetch_all_bots_by_user_id`
             title="Test Alias",
             description="Test Alias Description",
-            original_bot_id="public1",
+            original_bot_id=original_bot_id,
             last_used_time=1627984879.9,
             create_time=1627984879.9,
-            # Pinned
-            is_pinned=True,
+            is_pinned=is_pinned,
             sync_status="RUNNING",
             has_knowledge=True,
         )
-        alias2 = BotAliasModel(
-            id="alias2",
-            title="Test Alias",
-            description="Test Alias Description",
-            original_bot_id="public2",
-            last_used_time=1627984879.9,
-            create_time=1627984879.9,
-            # Not Pinned
-            is_pinned=False,
-            sync_status="RUNNING",
-            has_knowledge=True,
-        )
-        store_bot("user1", bot1)
-        store_bot("user1", bot2)
-        store_bot("user1", bot3)
-        store_bot("user1", bot4)
-        store_bot("user2", public_bot1)
-        store_bot("user2", public_bot2)
-        update_bot_visibility("user2", "public1", True)
-        update_bot_visibility("user2", "public2", True)
-        store_alias("user1", alias1)
-        store_alias("user1", alias2)
-        update_bot_publication("user2", "public1", "api1", "build1")
+class TestFindAllBots(unittest.IsolatedAsyncioTestCase):
+    first_user_id = "user1"
+    second_user_id = "user2"
+
+    def setUp(self) -> None:
+        bot1 = create_private_bot("1", True, self.first_user_id) 
+        bot2 = create_private_bot("2", True,self.first_user_id)
+        bot3 = create_private_bot("3", False,self.first_user_id)
+        bot4 = create_private_bot("4",False,self.first_user_id)
+        
+        public_bot1 = create_public_bot("public1",True,self.second_user_id)
+        public_bot2 = create_public_bot("public2",True,self.second_user_id)
+        
+        alias1 = create_bot_alias("alias1","public1",True)
+        alias2 = create_bot_alias("alias2","public2",False)
+        
+        store_bot(self.first_user_id, bot1)
+        store_bot(self.first_user_id, bot2)
+        store_bot(self.first_user_id, bot3)
+        store_bot(self.first_user_id, bot4)
+        store_bot(self.second_user_id, public_bot1)
+        store_bot(self.second_user_id, public_bot2)
+        update_bot_visibility(self.second_user_id, "public1", True)
+        update_bot_visibility(self.second_user_id, "public2", True)
+        store_alias(self.first_user_id, alias1)
+        store_alias(self.first_user_id, alias2)
+        update_bot_publication(self.second_user_id, "public1", "api1", "build1")
 
     def tearDown(self) -> None:
-        delete_bot_by_id("user1", "1")
-        delete_bot_by_id("user1", "2")
-        delete_bot_by_id("user1", "3")
-        delete_bot_by_id("user1", "4")
-        delete_bot_by_id("user2", "public1")
-        delete_bot_by_id("user2", "public2")
-        delete_alias_by_id("user1", "alias1")
-        delete_alias_by_id("user1", "alias2")
+        delete_bot_by_id(self.first_user_id, "1")
+        delete_bot_by_id(self.first_user_id, "2")
+        delete_bot_by_id(self.first_user_id, "3")
+        delete_bot_by_id(self.first_user_id, "4")
+        delete_bot_by_id(self.second_user_id, "public1")
+        delete_bot_by_id(self.second_user_id, "public2")
+        delete_alias_by_id(self.first_user_id, "alias1")
+        delete_alias_by_id(self.first_user_id, "alias2")
 
     def test_limit(self):
         # Private + public bots
