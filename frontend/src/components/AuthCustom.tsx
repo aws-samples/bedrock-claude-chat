c31a3329c96e9a1d8595fbecc1390fdd7781223e
statefb <tmgf.15.su@gmail.com>
2024-04-05 16:57:50 +0900

    fix

--- frontend/src/components/AuthCustom.tsx
+++ frontend/src/components/AuthCustom.tsx
@@ -5,10 +5,11 @@ import React, {
   cloneElement,
   ReactElement,
 } from 'react';
+import Button from './Button';
 import { BaseProps } from '../@types/common';
 import { Auth } from 'aws-amplify';
-import { Button, Text, Loader } from '@aws-amplify/ui-react';
 import { useTranslation } from 'react-i18next';
+import { PiCircleNotch } from 'react-icons/pi';
 
 type Props = BaseProps & {
   children: ReactNode;
@@ -45,17 +46,18 @@ const AuthCustom: React.FC<Props> = ({ children }) => {
   return (
     <>
       {loading ? (
-        <div className="grid grid-cols-1 justify-items-center gap-4">
-          <Text className="mt-12 text-center">Loading...</Text>
-          <Loader width="5rem" height="5rem" />
+        <div className="flex flex-col items-center p-4">
+          <div className="mb-3 text-4xl">Loading...</div>
+          <div className="animate-spin">
+            <PiCircleNotch size={100} />
+          </div>
         </div>
       ) : !authenticated ? (
-        <div className="grid grid-cols-1 justify-items-center gap-4">
-          <Text className="mt-12 text-center text-3xl">{t('app.name')}</Text>
-          <Button
-            variation="primary"
-            onClick={() => signIn()}
-            className="mt-6 w-60">
+        <div className="flex flex-col items-center gap-4">
+          <div className="mb-5 mt-10 text-4xl text-aws-sea-blue">
+            {t('app.name')}
+          </div>
+          <Button onClick={() => signIn()} className="px-20 text-xl">
             {t('signIn.button.login')}
           </Button>
         </div>
