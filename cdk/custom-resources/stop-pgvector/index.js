29ae2920800fdcd1631a9ec70913f847f0efd1b9
2814109 <2814109k@gmail.com>
2024-04-08 14:07:45 +0900

    add lambda

---
+++ cdk/custom-resources/stop-pgvector/index.js
@@ -0,0 +1,16 @@
+const AWS = require("aws-sdk");
+const rds = new AWS.RDS();
+
+exports.handler = async (event) => {
+  const instanceId = process.env.RDS_INSTANCE_ID;
+
+  try {
+    const stopData = await rds
+      .stopDBInstance({ DBInstanceIdentifier: instanceId })
+      .promise();
+    console.log(`RDS instance ${instanceId} stopped successfully.`, stopData);
+  } catch (err) {
+    console.error(`Error stopping RDS instance ${instanceId}:`, err);
+    throw err;
+  }
+};
