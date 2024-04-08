86aa0c93811bbd7ecc0a702bc0f44cac3e33e8ab
2814109 <2814109k@gmail.com>
2024-04-08 16:58:05 +0900

    update type

---
+++ cdk/lib/utils/corn.ts
@@ -0,0 +1,43 @@
+type CronFieldBase = string | number | "*" | "-" | ",";
+
+type Minute = `${CronFieldBase}` | "/";
+type Hour = `${CronFieldBase}` | "/";
+type DayOfMonth = `${CronFieldBase}` | "?" | "L" | "W" | "/";
+type Month =
+  | `${CronFieldBase}`
+  | "JAN"
+  | "FEB"
+  | "MAR"
+  | "APR"
+  | "MAY"
+  | "JUN"
+  | "JUL"
+  | "AUG"
+  | "SEP"
+  | "OCT"
+  | "NOV"
+  | "DEC"
+  | "/";
+type DayOfWeek =
+  | `${CronFieldBase}`
+  | "?"
+  | "L"
+  | "#"
+  | "SUN"
+  | "MON"
+  | "TUE"
+  | "WED"
+  | "THU"
+  | "FRI"
+  | "SAT"
+  | "/";
+type Year = `${1970 | number}-${2199 | number}` | "*" | "-" | "/";
+
+type CronSchedule = {
+  minute: Minute;
+  hour: Hour;
+  dayOfMonth: DayOfMonth;
+  month: Month;
+  dayOfWeek: DayOfWeek;
+  year: Year;
+};
