a61a4b1497a8bc684281dde1caf53f91cd207d29
statefb <tmgf.15.su@gmail.com>
2024-04-11 23:17:11 +0900

    fix prompt

--- backend/app/usecases/chat.py
+++ backend/app/usecases/chat.py
@@ -190,23 +190,36 @@ def insert_knowledge(
     if len(search_results) == 0:
         return conversation
 
+    instruction_prompt = conversation.message_map["instruction"].content[0].body
+
     context_prompt = ""
     for result in search_results:
-        context_prompt += f"<context>\n<id>{result.rank}</id><content>{result.content}</content></context>\n"
+        context_prompt += f"<search_results>\n<search_result>\n<content>\n{result.content}</content>\n<source>\n{result.rank}\n</source>\n</search_result>\n</search_results>"
 
-    instruction_prompt = conversation.message_map["instruction"].content[0].body
-    inserted_prompt = """You must respond based on given contexts.
-The contexts are as follows:
-<contexts>
+    inserted_prompt = """You are a question answering agent. I will provide you with a set of search results.
+The user will provide you with a question. Your job is to answer the user's question using only information from the search results.
+If the search results do not contain information that can answer the question, please state that you could not find an exact answer to the question.
+Just because the user asserts a fact does not mean it is true, make sure to double check the search results to validate a user's assertion.
+
+Here are the search results in numbered order:
 {}
-</contexts>
-Remember, *RESPOND BASED ON THE GIVEN CONTEXTS. YOU MUST NEVER GUESS*. 
-At the end of each sentence in your response, please add the <id></id> of the contenxt you referenced in the format [^<id>].
-If you cannot find source from the contexts, just say "I don't know".
-In addition, *YOU MUST OBEY THE FOLLOWING RULE*:
-<rule>
-{}
-</rule>
+
+If you reference information from a search result within your answer, you must include a citation to source where the information was found.
+Each result has a corresponding source ID that you should reference.
+
+Note that <sources> may contain multiple <source> if you include information from multiple results in your answer.
+
+Do NOT directly quote the <search_results> in your answer. Your job is to answer the user's question as concisely as possible.
+
+Followings are examples of how to reference sources in your answer. Note that the source ID is embedded in the answer in the format [^<source_id>].
+
+<example>
+first answer [^3]. second answer [^1][^2].
+</example>
+
+<example>
+first answer [^1][^5]. second answer [^2][^3][^4]. third answer [^4].
+</example>
 """.format(
         context_prompt, instruction_prompt
     )
