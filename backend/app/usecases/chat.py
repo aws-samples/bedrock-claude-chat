05d45e5d9406f77672645eaf04cdb3f77f887ad5
statefb <tmgf.15.su@gmail.com>
2024-04-12 15:23:54 +0900

    fix prompt

--- backend/app/usecases/chat.py
+++ backend/app/usecases/chat.py
@@ -190,17 +190,13 @@ def insert_knowledge(
     if len(search_results) == 0:
         return conversation
 
-    # Currently, we not use instruction prompt when inserting knowledge.
-    # If needed, must be verified by putting the instruction prompt in the following `inserted_prompt`.
-    # instruction_prompt = conversation.message_map["instruction"].content[0].body
-
     instruction_prompt = conversation.message_map["instruction"].content[0].body
 
     context_prompt = ""
     for result in search_results:
         context_prompt += f"<search_result>\n<content>\n{result.content}</content>\n<source>\n{result.rank}\n</source>\n</search_result>"
 
-    inserted_prompt = """You are a question answering agent. I will provide you with a set of search results.
+    inserted_prompt = """You are a question answering agent. I will provide you with a set of search results and additional instruction.
 The user will provide you with a question. Your job is to answer the user's question using only information from the search results.
 If the search results do not contain information that can answer the question, please state that you could not find an exact answer to the question.
 Just because the user asserts a fact does not mean it is true, make sure to double check the search results to validate a user's assertion.
@@ -210,13 +206,18 @@ Here are the search results in numbered order:
 {}
 </search_results>
 
+Here is the additional instruction:
+<additional-instruction>
+{}
+</additional-instruction>
+
 If you reference information from a search result within your answer, you must include a citation to source where the information was found.
 Each result has a corresponding source ID that you should reference.
 
 Note that <sources> may contain multiple <source> if you include information from multiple results in your answer.
 
 Do NOT directly quote the <search_results> in your answer. Your job is to answer the user's question as concisely as possible.
-Do NOT provide a source at the end of your answer.
+Do NOT provide sources at the end of your answer.
 
 Followings are examples of how to reference sources in your answer. Note that the source ID is embedded in the answer in the format [^<source_id>].
 
@@ -233,15 +234,8 @@ first answer [^1].
 
 [^1]: https://example.com
 </BAD-example>
-
-In addition, *YOU MUST OBEY THE FOLLOWING RULE*:
-<rule>
-{}
-</rule>
-
 """.format(
-        context_prompt,
-        instruction_prompt
+        context_prompt, instruction_prompt
     )
     logger.info(f"Inserted prompt: {inserted_prompt}")
 
