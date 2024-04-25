19280e4e8475354a5a4cf9acf6db8636726afca4
fsatsuki <fsatsuki@amazon.co.jp>
2024-04-25 00:40:25 +0000

    レビューコメント反映

--- backend/app/usecases/bot.py
+++ backend/app/usecases/bot.py
@@ -212,24 +212,9 @@ def modify_owned_bot(
     # 'sync_status= "SUCCEEDED"' will update only dynamodb record.
     bot = find_private_bot_by_id(user_id, bot_id)
     sync_status = (
-        "SUCCEEDED"
-        if (
-            modify_input.knowledge is not None
-            and bot.knowledge is not None
-            and sorted(modify_input.knowledge.source_urls)
-            == sorted(bot.knowledge.source_urls)
-            and sorted(modify_input.knowledge.sitemap_urls)
-            == sorted(bot.knowledge.sitemap_urls)
-            and len(modify_input.knowledge.added_filenames) == 0
-            and len(modify_input.knowledge.deleted_filenames) == 0
-            and modify_input.embedding_params is not None
-            and bot.embedding_params is not None
-            and modify_input.embedding_params.chunk_size
-            == bot.embedding_params.chunk_size
-            and modify_input.embedding_params.chunk_overlap
-            == bot.embedding_params.chunk_overlap
-        )
-        else "QUEUED"
+        "QUEUED"
+        if modify_input.is_embedding_required(bot)
+        else "SUCCEEDED"
     )
 
     update_bot(
