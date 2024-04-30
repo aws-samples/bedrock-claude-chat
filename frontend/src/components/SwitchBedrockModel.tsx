704cd98d92801decb8b07212f07c4636e0301f83
Akashpeter3 <52629290+Akashpeter3@users.noreply.github.com>
2024-04-30 18:32:21 +0530

    Update SwitchBedrockModel.tsx

--- frontend/src/components/SwitchBedrockModel.tsx
+++ frontend/src/components/SwitchBedrockModel.tsx
@@ -15,17 +15,17 @@ const SwitchBedrockModel: React.FC<Props> = (props) => {
         'flex justify-center gap-2 rounded-lg border border-light-gray bg-light-gray p-1 text-sm'
       )}>
       {availableModels.map((availableModel) => (
-        <Button
-          key={availableModel.modelId}
-          className={twMerge(
-            'flex w-40 flex-1 items-center rounded-lg p-2',
-            modelId === availableModel.modelId
-              ? ''
-              : 'border-light-gray bg-white text-dark-gray'
-          )}
-          onClick={() => setModelId(availableModel.modelId)}
-          children={<span>{availableModel.label}</span>}
-        />
+        // <Button
+        //   key={availableModel.modelId}
+        //   className={twMerge(
+        //     'flex w-40 flex-1 items-center rounded-lg p-2',
+        //     modelId === availableModel.modelId
+        //       ? ''
+        //       : 'border-light-gray bg-white text-dark-gray'
+        //   )}
+        //   onClick={() => setModelId(availableModel.modelId)}
+        //   children={<span>{availableModel.label}</span>}
+        // />
       ))}
     </div>
   );
