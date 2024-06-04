fe84c86268f14002e7334ce6c510f23cef4fc5c0
2814109 <2814109k@gmail.com>
2024-06-04 10:37:27 +0900

    update comment : why use logistic curve

--- frontend/src/features/agent/functions/logisticCurve.ts
+++ frontend/src/features/agent/functions/logisticCurve.ts
@@ -1,7 +1,5 @@
 /* Adopting the logistic curve for the following reasons:
- * - It allows for representing progress without a defined maximum number of events.
- * - It is understood that the task typically concludes after approximately five events.
- * - The curve's initial slow progression that accelerates mid-way enhances the user experience (UX).
+ * The agent takes time to handle complex tasks, but it improves the user's waiting experience by displaying progress using a logistic curve based on the number of actions. This curve shows a slow initial change, a rapid change in the middle, and eventually stabilizes.
  */
 export const logisticCurve = (x: number) => {
   const L = 1; // The maximum value
