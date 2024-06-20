380d29c3635a51ed10e824d19a008ad6aae11e4d
2814109 <2814109k@gmail.com>
2024-06-20 11:15:39 +0900

    add eslint rule

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -9,8 +9,9 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'eslint-plugin-react-compiler'],
   rules: {
+    'react-compiler/react-compiler': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
