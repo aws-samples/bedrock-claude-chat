a0a01d54ce58b2efd81dfa87498bf40067acb2a3
2814109 <2814109k@gmail.com>
2024-06-25 13:55:53 +0900

    fix vite-pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true,
+        type: 'module',
       },
       injectRegister: 'auto',
       manifest: {
