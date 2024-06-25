4abab722cc2b3ef1d0b5b72d1d04fb2b831e4bb4
Satoshi Watanabe <watsatos@amazon.co.jp>
2024-06-25 09:47:48 +0900

    imple the feature of attach text file

--- frontend/src/components/ChatMessage.tsx
+++ frontend/src/components/ChatMessage.tsx
@@ -157,11 +157,17 @@ const ChatMessage: React.FC<Props> = (props) => {
                   );
                 } else {
                   return (
-                    <React.Fragment key={idx}>
-                      {content.body.split('\n').map((c, idxBody) => (
-                        <div key={idxBody}>{c}</div>
-                      ))}
-                    </React.Fragment>
+                    // <React.Fragment key={idx}>
+                    //   {content.body.split('\n').map((c, idxBody) => (
+                    //     <div key={idxBody}>{c}</div>
+                    //   ))}
+                    // </React.Fragment>
+                    //// [Customize]インプットメッセージもMarkdown書式で整形表示できるよう修正(ファイルアイコンにする場合は、ここは見直し)
+                    <ChatMessageMarkdown
+                      key={idx}
+                      messageId={String(idx)}>
+                      {content.body}
+                    </ChatMessageMarkdown>    
                   );
                 }
               })}
