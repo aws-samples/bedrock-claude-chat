00c73e612b5984cd9921306c8415f0bd3fad2dd5
2814109 <2814109k@gmail.com>
2024-07-01 23:55:25 +0900

    add stories

---
+++ frontend/src/components/ButtonFileChoose.stories.tsx
@@ -0,0 +1,8 @@
+import { TbPhotoPlus } from 'react-icons/tb';
+import ButtonFileChoose from './ButtonFileChoose';
+
+export const Ideal = () => (
+  <ButtonFileChoose className="w-10" icon onChange={() => {}}>
+    <TbPhotoPlus />
+  </ButtonFileChoose>
+);
