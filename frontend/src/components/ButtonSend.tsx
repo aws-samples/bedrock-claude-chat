71a8c86777802674c9acc67924a3dc6d5349acad
2814109 <2814109k@gmail.com>
2024-07-01 22:22:29 +0900

    fix button

--- frontend/src/components/ButtonSend.tsx
+++ frontend/src/components/ButtonSend.tsx
@@ -13,7 +13,7 @@ const ButtonSend: React.FC<Props> = (props) => {
   return (
     <button
       className={twMerge(
-        'flex items-center justify-center rounded-xl bg-white p-2 text-xl text-aws-sea-blue hover:bg-aws-sea-blue-hover hover:text-white',
+        'flex items-center justify-center rounded-xl bg-aws-sea-blue  p-2 text-xl  text-white hover:bg-aws-sea-blue-hover',
         props.disabled ? 'opacity-30' : '',
         props.className
       )}
