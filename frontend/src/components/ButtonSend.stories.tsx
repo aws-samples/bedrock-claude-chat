f2f97935cd4591334833b57b130ee741ecf4070b
2814109 <2814109k@gmail.com>
2024-07-01 23:57:01 +0900

    add atoms

---
+++ frontend/src/components/ButtonSend.stories.tsx
@@ -0,0 +1,7 @@
+import ButtonSend from './ButtonSend';
+
+export const Ideal = () => <ButtonSend onClick={() => {}} />;
+
+export const Loading = () => <ButtonSend loading onClick={() => {}} />;
+
+export const Diabled = () => <ButtonSend disabled onClick={() => {}} />;
